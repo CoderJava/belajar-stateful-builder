e962585421c984df926d90ed858a61d07aab64da
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-05-21 10:15:47 +0700

    Buat contoh perbandingan menggunakan StatefulWidget dan StatefulBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,276 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Belajar Stateful Builder',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final listRingtones = <String>[
+    'None',
+    'Callisto',
+    'Ganymede',
+    'Luna',
+    'Mash-up',
+    'School\'s out',
+    'Zen too',
+  ];
+  late String selectedRingtone;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    selectedRingtone = listRingtones.first;
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    debugPrint('build parent');
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Belajar Stateful Builder'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: InkWell(
+        onTap: () async {
+          final resultSelectedRingtone = await showDialog<String?>(
+            context: context,
+            builder: (context) {
+              // Versi 1 menggunakan class StatefulWidget
+              /*return DialogPhoneRingtone(
+                listRingtones: listRingtones,
+                defaultSelectedRingtone: selectedRingtone,
+              );*/
+
+              // Versi 2 menggunakan StatefulBuilder
+              return buildDialogPhoneRingtone();
+            },
+          );
+          if (resultSelectedRingtone != null) {
+            setState(() {
+              selectedRingtone = resultSelectedRingtone;
+            });
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Phone ringtone',
+                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                        fontWeight: FontWeight.w500,
+                      ),
+                ),
+                Text(
+                  selectedRingtone,
+                  style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                        color: Colors.grey,
+                      ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget buildDialogPhoneRingtone() {
+    return StatefulBuilder(
+      builder: (context, setState) {
+        debugPrint('build stateful builder');
+        final mediaQueryData = MediaQuery.of(context);
+        final widthScreen = mediaQueryData.size.width;
+        final heightScreen = mediaQueryData.size.height;
+        return AlertDialog(
+          title: const Text(
+            'Phone ringtone',
+            textAlign: TextAlign.start,
+          ),
+          titlePadding: const EdgeInsets.only(
+            left: 16,
+            top: 16,
+          ),
+          contentPadding: const EdgeInsets.all(16),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: widthScreen / 1.2,
+                height: heightScreen / 2,
+                child: ListView(
+                  shrinkWrap: true,
+                  children: listRingtones.map((element) {
+                    return Row(
+                      children: [
+                        Radio<String>(
+                          value: element,
+                          groupValue: selectedRingtone,
+                          onChanged: (_) {
+                            setState(() {
+                              selectedRingtone = element;
+                            });
+                          },
+                        ),
+                        Text(
+                          element,
+                          style: Theme.of(context).textTheme.caption?.copyWith(
+                                color: Colors.grey[900],
+                              ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    child: const Text('CANCEL'),
+                    style: TextButton.styleFrom(
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('OK'),
+                    style: TextButton.styleFrom(
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context, selectedRingtone);
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class DialogPhoneRingtone extends StatefulWidget {
+  final List<String> listRingtones;
+  final String defaultSelectedRingtone;
+
+  const DialogPhoneRingtone({
+    Key? key,
+    required this.listRingtones,
+    required this.defaultSelectedRingtone,
+  }) : super(key: key);
+
+  @override
+  State<DialogPhoneRingtone> createState() => _DialogPhoneRingtoneState();
+}
+
+class _DialogPhoneRingtoneState extends State<DialogPhoneRingtone> {
+  late String selectedRingtone;
+
+  @override
+  void initState() {
+    selectedRingtone = widget.defaultSelectedRingtone;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryData = MediaQuery.of(context);
+    final widthScreen = mediaQueryData.size.width;
+    final heightScreen = mediaQueryData.size.height;
+    return AlertDialog(
+      title: const Text(
+        'Phone ringtone',
+        textAlign: TextAlign.start,
+      ),
+      titlePadding: const EdgeInsets.only(
+        left: 16,
+        top: 16,
+      ),
+      contentPadding: const EdgeInsets.all(16),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(
+            width: widthScreen / 1.2,
+            height: heightScreen / 2,
+            child: ListView(
+              shrinkWrap: true,
+              children: widget.listRingtones.map((element) {
+                return Row(
+                  children: [
+                    Radio<String>(
+                      value: element,
+                      groupValue: selectedRingtone,
+                      onChanged: (_) {
+                        setState(() {
+                          selectedRingtone = element;
+                        });
+                      },
+                    ),
+                    Text(
+                      element,
+                      style: Theme.of(context).textTheme.caption?.copyWith(
+                            color: Colors.grey[900],
+                          ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                child: const Text('CANCEL'),
+                style: TextButton.styleFrom(
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              TextButton(
+                child: const Text('OK'),
+                style: TextButton.styleFrom(
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ),
+                onPressed: () {
+                  Navigator.pop(context, selectedRingtone);
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
